8c0acad3465a43c12033ba748c85423bbd27dc96
Roy Mosby <mosbyroy25@gmail.com>
2018-10-16 00:16:37 -0700

    Cant complete any questions

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,9 +1,26 @@
 # Exercise 4: functions and conditionals
-
+library(stringr)
 # Define a function `is_twice_as_long` that takes in two character strings, and 
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
+is_twice_as_long <- function(one,two){
+ if(str_length(one) >str_length(two)){
+   as.numeric(smallest) <- two
+   if(str_length(one==smallest *2)){
+     return("The length of string one is equal to twice the length of the other")
+   }else{
+     return("The length of string one is greater than the second length")
+   }
+ }else{
+   as.numeric(smallest) <- one
+   if(str_length(two)==smallest){
+     return("The length of string two is equal to twice the length of the other")
+   }else{
+     return("The length of string one is greater than the second length")
+   }
+ }
+}
 
 
 # Call your `is_twice_as_long` function by passing it different length strings
